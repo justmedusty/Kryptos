bd27d3e6921a4ecb026c778600e21f5fd6cd4eea
Dustyn <dustyn_gibb@hotmail.com>
2025-01-30 15:26:13 -0500

    added macro for array init in crypto tests

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -2,24 +2,31 @@
 mod cryptography_tests {
     use crate::cryptography::aes::*;
     use crate::cryptography::cryptography::Encryption;
-    use crate::cryptography::rc4::{Rc4State, KEY_SIZE_BYTES};
+    use crate::cryptography::rc4::Rc4State;
 
+    macro_rules! generate_ab_arrays {
+        ($size:expr) => {{
+            let mut input = [0u8; $size];
+            let mut output = [0u8; $size];
+
+            for i in 0..$size {
+                if i % 2 == 0 {
+                    input[i] = b'B';
+                    output[i] = b'B';
+                } else {
+                    input[i] = b'A';
+                    output[i] = b'A';
+                }
+            }
+
+            (input, output)
+        }};
+    }
     ///This test just ensures that the encryption function does in fact encrypt the plaintext input
     #[test]
     fn test_rc4_encryption() {
         let mut rc4 = Rc4State::new();
-        let mut input = [0; 256];
-        let mut output = [0; 256];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (input, mut output) = generate_ab_arrays!(256);
         assert_eq!(input, output);
         rc4.encrypt(&input, &mut output);
 
@@ -30,16 +37,7 @@ mod cryptography_tests {
     #[test]
     fn test_rc4_decryption() {
         let mut rc4 = Rc4State::new();
-        let mut input = [0; KEY_SIZE_BYTES];
-        let mut output = [0; KEY_SIZE_BYTES];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                continue;
-            }
-            input[i] = 'A' as u8;
-        }
+        let (mut input, mut output) = generate_ab_arrays!(256);
 
         let original_input = input.clone();
         rc4.encrypt(&input, &mut output);
@@ -51,18 +49,7 @@ mod cryptography_tests {
     #[test]
     fn test_aes_cbc_encryption() {
         let mut aes = AESContext::new(AesMode::CBC, AesSize::S128, None);
-        let mut input = [0; 256];
-        let mut output = [0; 256];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (input, mut output) = generate_ab_arrays!(256);
         assert_eq!(input, output);
         aes.encrypt(&input, &mut output);
 
@@ -72,18 +59,7 @@ mod cryptography_tests {
     #[test]
     fn test_aes_cbc_decryption() {
         let mut aes = AESContext::new(AesMode::CBC, AesSize::S256, None);
-        let mut input = [0; 256];
-        let mut output = [0; 256];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (mut input, mut output) = generate_ab_arrays!(256);
 
         let original_input = input.clone();
         assert_eq!(input, output);
@@ -101,22 +77,10 @@ mod cryptography_tests {
         assert_eq!(input, original_input);
     }
 
-
     #[test]
     fn test_aes_ecb_encryption() {
         let mut aes = AESContext::new(AesMode::ECB, AesSize::S256, None);
-        let mut input = [0; 16];
-        let mut output = [0; 16];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (input, mut output) = generate_ab_arrays!(256);
         assert_eq!(input, output);
         aes.encrypt(&input, &mut output);
         assert_ne!(input, output);
@@ -125,76 +89,31 @@ mod cryptography_tests {
     #[test]
     fn test_aes_ecb_decryption() {
         let mut aes = AESContext::new(AesMode::ECB, AesSize::S256, None);
-        let mut input = [0; 32];
-        let mut output = [0; 32];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (mut input, mut output) = generate_ab_arrays!(256);
         let original_input = input.clone();
         aes.encrypt(&input, &mut output);
         aes.decrypt(&output, &mut input);
         assert_eq!(input, original_input);
     }
 
-
     #[test]
     fn test_aes_ctr_encryption() {
         let mut aes = AESContext::new(AesMode::CTR, AesSize::S256, None);
-        let mut input = [0; 256];
-        let mut output = [0; 256];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
+        let (input, mut output) = generate_ab_arrays!(256);
         assert_eq!(input, output);
         aes.encrypt(&input, &mut output);
-
         assert_ne!(input, output);
     }
 
     #[test]
     fn test_aes_ctr_decryption() {
         let mut aes = AESContext::new(AesMode::CTR, AesSize::S128, None);
-        let mut input = [0; 256];
-        let mut output = [0; 256];
-
-        for i in 0..input.len() {
-            if i % 2 == 0 {
-                input[i] = 'B' as u8;
-                output[i] = 'B' as u8;
-                continue;
-            }
-            output[i] = 'A' as u8;
-            input[i] = 'A' as u8;
-        }
-
+        let (mut input, mut output) = generate_ab_arrays!(256);
         let original_input = input.clone();
         assert_eq!(input, output);
         aes.encrypt(&input, &mut output);
-        for byte in output.iter().by_ref() {
-            print!("{:02x} ", byte);
-        }
-        println!(" ");
         assert_ne!(input, output);
         aes.decrypt(&output, &mut input);
-
-        for byte in output.iter().by_ref() {
-            print!("{:02x} ", byte);
-        }
         assert_eq!(input, original_input);
     }
-
 }
